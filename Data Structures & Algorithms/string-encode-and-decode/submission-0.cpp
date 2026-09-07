ead845391cb556df39782c27a25d5671374959fe
Vanam Gatoj Kumar <gatojkumar@gmail.com>
2026-09-07 23:04:56 +0530

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string encode(vector<string>& strs) {
+        string encoded = "";
+        for (const string& s : strs) {
+            encoded += to_string(s.size()) + "#" + s;
+        }
+        return encoded;
+    }
+    vector<string> decode(string s) {
+        vector<string> decoded;
+        int i = 0;
+        int n = s.size();
+        while (i < n) {
+            int j = i;
+            while (s[j] != '#') {
+                j++;
+            }
+            int len = stoi(s.substr(i, j - i));
+            string str = s.substr(j + 1, len);
+            decoded.push_back(str);
+            i = j + 1 + len;
+        }
+
+        return decoded;
+    }
+};
